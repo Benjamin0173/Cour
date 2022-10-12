4b32110d7c352be03974e19ddbdbdb4fd973875d
Benjamin <123456benjamin@gmail.com>
2022-10-12 11:35:47 +0200

    third commit

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,6 +1,5 @@
 import { useState } from 'react'; 
  
- // 
 const useInput = (validateValue) => { 
   const [enteredValue, setEnteredValue] = useState(''); 
   const [isTouched, setIsTouched] = useState(false); 
